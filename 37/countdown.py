28d657c4346e8c5fc8a4d05bd944db0263f1d8d4
lamp41 <lampqwee@DESKTOP-R6H5PFR.localdomain>
2019-11-14 09:04:40 +0300

    37.1

--- 37/countdown.py
+++ 37/countdown.py
@@ -1,8 +1,25 @@
 def countdown_for(start=10):
     for i in reversed(range(1, start + 1)):
         print(i)
-    print('time is up')
+    print("time is up")
 
 
 def countdown_recursive(start=10):
-    pass
+    if(countdown_recursive.isfirst = True):
+        if (start > 0):
+            countdown_recursive.count = start
+            countdown_recursive.isfirst = False
+            print(start)
+            continue
+        else:
+            print('time is up')
+    if countdown_recursive.isfirst = False:
+        countdown_recursive.count -= 1
+        if(countdown_recursive.count > 1):
+            print(countdown_recursive.count)
+            continue
+        else:
+            print('time is up')
+
+    else:
+        countdown_recursive.isfirst = True
